0a7b5af22248bf79ab7dac43251cb1e6bf19a206
aswathamaallu007-star <aswathamaallu786@gmail.com>
2025-09-17 23:06:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const sounds = document.querySelectorAll('.btn')
+const stops = document.querySelector('.stop')
+sounds.forEach(button => {
+	button.addEventListener('click',playsound)
+})
+stops.addEventListener('click',stopsound)
+const playlist = {}
+function playsound(e){
+	const box = e.target.id
+	const audio = new Audio(`sounds/${box}.mp3`)
+	playlist[box] = audio;
+	stopsound();
+	audio.play();
+}
+function stopsound(){
+	for(let key in playlist){
+		const audio = playlist[key]
+		audio.currentTime = 0;
+		audio.pause();
+		
+	}
+}
